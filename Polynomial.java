276c0142d40a5a78da2954c8554fe49b9abd2a5e
Rhodwell <rjb.malicdem@gmail.com>
2023-09-13 20:06:50 -0400

    added Polynomial.java and Driver.java

--- Polynomial.java
+++ Polynomial.java
@@ -3,36 +3,44 @@
  * September 13, 2023
  * CSCB07: Lab 1
 */
-public class Polynomial {
+public class Polynomial
+{
 	private double[] coefficient;
 
-	public Polynomial() {
+	public Polynomial()
+	{
 		coefficient = new double[1];
 		coefficient[0] = 0.0;
 	}// end Polynomial()
 
-	public Polynomial(double[] coefficient) {
+	public Polynomial(double[] coefficient)
+	{
 		this.coefficient = coefficient.clone();
 	}// end Polynomial()
 
-	public Polynomial add(Polynomial poly) {
+	public Polynomial add(Polynomial poly)
+	{
 		double[] sum = poly.coefficient.clone();
-		for (int i = 0; i < coefficient.length; i++) {
+		for (int i = 0; i < coefficient.length; i++)
+		{
 			sum[i] += coefficient[i];
 		} // end loop
 		Polynomial newPoly = new Polynomial(sum);
 		return newPoly;
 	}// end add()
 
-	public double evaluate(double xValue) {
+	public double evaluate(double xValue)
+	{
 		double result = 0;
-		for (int i = 0; i < coefficient.length; i++) {
+		for (int i = 0; i < coefficient.length; i++)
+		{
 			result += coefficient[i] * Math.pow(xValue, i);
 		} // end loop
 		return result;
 	}// end evaluate()
 
-	public boolean hasRoot(double xRoot) {
+	public boolean hasRoot(double xRoot)
+	{
 		return evaluate(xRoot) == 0;
 	}// end hasRoot()
 }
