e85c5cc8e95b52f8184f6d43de41c2ed7d25bdeb
Rhodwell <rjb.malicdem@gmail.com>
2023-10-01 18:58:39 -0400

    modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -1,18 +1,57 @@
+/*
+ * Rhodwell Jireh Malicdem
+ * October 1, 2023
+ * CSCB07: Driver - Lab 2
+ */
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 public class Driver 
 {
-	public static void main(String[] args)
+	public static void main(String[] args) throws FileNotFoundException 
 	{
-		Polynomial p = new Polynomial();
-		System.out.println(p.evaluate(3));
-		double[] c1 = { 6, 0, 0, 5 };
-		Polynomial p1 = new Polynomial(c1);
-		double[] c2 = { 0, -2, 0, 0, -9 };
-		Polynomial p2 = new Polynomial(c2);
-		Polynomial s = p1.add(p2);
-		System.out.println("s(0.1) = " + s.evaluate(0.1));
-		if (s.hasRoot(1))
-			System.out.println("1 is a root of s");
-		else
-			System.out.println("1 is not a root of s");
+		double[] coefficient = {2.1, -1, 4.3};
+		int[] exponent = {0, 5, 3};
+		File testFile = new File("C:\\Eclipse\\CSCB07\\Equation.txt");
+		//Testing Constructors
+		Polynomial noArgument = new Polynomial();
+		Polynomial twoArguments = new Polynomial(coefficient, exponent);
+		Polynomial fileArgument = new Polynomial(testFile);
+		//Testing Polynomial.add()
+		System.out.println("Addition");
+		System.out.println("--------");
+		System.out.println("No Argument + Two Arguments: " + noArgument.add(twoArguments));
+		System.out.println("No Argument + File Argument: " + noArgument.add(fileArgument));
+		System.out.println("Two Arguments + File Argument: " + twoArguments.add(fileArgument));
+		System.out.println("");
+		//Testing Polynomial.multply()
+		System.out.println("Multiplication");
+		System.out.println("--------------");
+		System.out.println("No Argument * Two Arguments: " + noArgument.multiply(twoArguments));
+		System.out.println("No Argument * File Arguments: " + noArgument.multiply(fileArgument));
+		System.out.println("Two Argument * File Arguments: " + twoArguments.multiply(fileArgument));
+		System.out.println();
+		//Testing Polynomial.evaluate()
+		System.out.println("Evaluate");
+		System.out.println("--------");
+		System.out.println("No Argument @ x=5: " + noArgument.evaluate(5));
+		System.out.println("Two Arguments @ x=5: " + twoArguments.evaluate(5));
+		System.out.println("File Arguments @ x=5: " + fileArgument.evaluate(5));
+		System.out.println();
+		//Testing Polynomial.hasRoot()
+		System.out.println("hasRoot");
+		System.out.println("--------");
+		System.out.println("No Argument @ x=5: " + noArgument.hasRoot(5));
+		System.out.println("Two Arguments @ x=5: " + twoArguments.hasRoot(5));
+		System.out.println("File Arguments @ x=5: " + fileArgument.hasRoot(5));
+		//Testing Polynomial.saveToFile()
+		try 
+		{
+			twoArguments.multiply(fileArgument).saveToFile("C:\\Eclipse\\CSCB07\\newerEquation.txt");
+		} 
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
 	}
 }
--- Polynomial.java
+++ Polynomial.java
@@ -1,48 +1,144 @@
 /*
  * Rhodwell Jireh Malicdem
  * September 13, 2023
- * CSCB07: Lab 1
+ * CSCB07: Lab 1 & Lab 2
 */
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
 public class Polynomial
 {
 	double[] coefficient;
+	int[] exponent;
 	public Polynomial()
 	{
-		coefficient = new double[1];
-		coefficient[0] = 0.0;
+		coefficient = new double[0];
+		exponent = new int[0];
 	}// end Polynomial()
-	public Polynomial(double[] coefficient)
+	public Polynomial(double[] coefficient, int[] exponent)
 	{
 		this.coefficient = coefficient.clone();
+		this.exponent = exponent.clone();
 	}// end Polynomial()
-	public Polynomial add(Polynomial poly)
+	public Polynomial(File file) throws FileNotFoundException
 	{
-		double[] sum;
-		if(poly.coefficient.length > coefficient.length)
+		int counter = 0;
+		Scanner fileRead = new Scanner(file);
+		String equation = "";
+		while(fileRead.hasNextLine())
 		{
-			sum = poly.coefficient.clone();
-			for (int i = 0; i < coefficient.length; i++)
-			{
-				sum[i] += coefficient[i];
-			} // end loop
-		}//end if
-		else
+			equation += fileRead.nextLine();
+		}
+		equation = equation.replace("-", "+-");
+		String[] splitConstant = equation.split("\\+");
+		for(String i: splitConstant)
 		{
-			sum = coefficient.clone();
-			for (int i = 0; i < poly.coefficient.length; i++)
+			if(i != "") counter++;
+		}
+		coefficient = new double[counter];
+		exponent = new int[counter];
+		counter = 0;
+		for(int i=0; i<splitConstant.length; i++)
+		{
+			if(splitConstant[i].contains("x"))
 			{
-				sum[i] += poly.coefficient[i];
-			} // end loop
-		}//end if
-		Polynomial newPoly = new Polynomial(sum);
-		return newPoly;
+				coefficient[counter] = Double.parseDouble(splitConstant[i].split("x")[0]);
+				exponent[counter] = Integer.parseInt(splitConstant[i].split("x")[1]);
+				counter++;
+			}
+			else if(splitConstant[i] != "")
+			{
+				coefficient[counter] = Double.parseDouble(splitConstant[i]);
+				exponent[counter] = 0;
+				counter++;
+			}//end if
+		}//end loop
+		fileRead.close();
+	}//end Polynomial()
+	public int getMax(int[] list)
+	{
+		int max = 0;
+		for(int i=0; i<list.length; i++)
+		{
+			if(list[i] > max) max = list[i];
+		}
+		return max;
+	}
+	public int getIndex(int[] list, int value)
+	{
+		for(int i=0; i<list.length; i++)
+		{
+			if(list[i] == value) return i;
+		}
+		return -1;
+	}
+	public Polynomial add(Polynomial newPoly)
+	{
+		int length = Math.max(getMax(exponent), getMax(newPoly.exponent)) + 1, counter = 0;
+		int[] tempExponent = new int[length];
+		double[] tempCoefficient = new double[length];
+		for(int i=0; i<length; i++)
+		{
+			tempExponent[i] = i;
+			tempCoefficient[i] = ((getIndex(exponent, i) != -1)?coefficient[getIndex(exponent, i)]:0.0) + ((getIndex(newPoly.exponent, i) != -1)?newPoly.coefficient[getIndex(newPoly.exponent, i)]:0.0);
+		}//end loop
+		for(double i: tempCoefficient)
+		{
+			if(i != 0.0) counter++;
+		}//end loop
+		int[] newExponent = new int[counter];
+		double[] newCoefficient = new double[counter];
+		counter = 0;
+		for(int i=0; i<length; i++)
+		{
+			if(tempCoefficient[i] != 0.0)
+			{
+				newCoefficient[counter] = tempCoefficient[i];
+				newExponent[counter] = tempExponent[i];
+				counter++;
+			}//end if
+		}//end loop
+		return new Polynomial(newCoefficient, newExponent);
 	}// end add()
+	public Polynomial multiply(Polynomial newPoly)
+	{
+		int counter = 0;
+		int[] indexCopy = new int[newPoly.exponent.length * exponent.length];
+		double[] tempCoefficient = new double[newPoly.coefficient.length * coefficient.length];
+		int[] tempExponent = new int[newPoly.exponent.length * exponent.length];
+		for(int i=0; i<newPoly.coefficient.length; i++)
+		{
+			for(int j=0; j<coefficient.length; j++)
+			{
+				tempCoefficient[counter] = newPoly.coefficient[i] * coefficient[j];
+				tempExponent[counter] = newPoly.exponent[i] + exponent[j];
+				counter++;
+			}//end loop
+		}//end loop
+		counter = 0;
+		for(int i=0; i<tempExponent.length-1; i++)
+		{
+			for(int j=i+1; j<tempExponent.length; j++)
+			{
+				if(tempExponent[i] == tempExponent[j] && getIndex(indexCopy, j) == -1)
+				{
+					tempCoefficient[i] += tempCoefficient[j];
+					tempCoefficient[j] = 0;
+					indexCopy[counter] = j;
+					counter++;
+				}//end if
+			}//end loop
+		}//end loop
+		return new Polynomial(tempCoefficient, tempExponent).add(new Polynomial());
+	}//end multiply()
 	public double evaluate(double xValue)
 	{
 		double result = 0;
 		for (int i = 0; i < coefficient.length; i++)
 		{
-			result += coefficient[i] * Math.pow(xValue, i);
+			result += coefficient[i] * Math.pow(xValue, exponent[i]);
 		} // end loop
 		return result;
 	}// end evaluate()
@@ -50,4 +146,25 @@ public class Polynomial
 	{
 		return evaluate(xRoot) == 0;
 	}// end hasRoot()
+	public void saveToFile(String fileName) throws IOException
+	{
+		String equation = "";
+		if(!new File(fileName).createNewFile()) new FileWriter(fileName, false).close();
+		try (FileWriter file = new FileWriter(fileName)) {
+			for(int i=0; i<coefficient.length; i++)
+			{
+				equation += ((exponent[i] == 0)? Math.round(coefficient[i]*10)/10.0: Math.round(coefficient[i]*10)/10.0 + "x" + exponent[i]) + ((i==exponent.length-1 || coefficient[i+1]<0)?"":"+");  
+			}//end loop
+			file.write(equation);
+		}
+	}//end saveToFile()
+	public String toString()
+	{
+		String equation = "";
+		for(int i=0; i<exponent.length; i++)
+		{
+			equation+=Math.round(coefficient[i]*10)/10.0+ "x" + exponent[i] + ((i==exponent.length-1 || coefficient[i+1]<0)?"":"+");
+		}//end loop
+		return equation;
+	}//end toString()
 }
